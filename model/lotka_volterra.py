8efc95682df6da6cc21ec87016f64e7be9643324
john-wang1015 <johnwang19931015@gmail.com>
2022-02-18 15:48:37 +1000

    model

--- model/lotka_volterra.py
+++ model/lotka_volterra.py
@@ -64,9 +64,5 @@ class Prior(BoxUniform):
         lower = np.full(4, -5.0)
         upper = np.full(4, +2.0)
         BoxUniform.__init__(self, lower, upper)
-<<<<<<< HEAD
-=======
 
 
-
->>>>>>> 8867edf (model)
